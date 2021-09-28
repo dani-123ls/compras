14e6b4a69c864152826b3c99027bacc0d5305fd3
Daniel <daniel.leon@o365.unab.edu.co>
2021-09-28 15:45:21 -0500

    Se crea una página de prueba para taiwind y se empieza a trabajar con JavaScript

---
+++ js/saludar.js
@@ -0,0 +1,7 @@
+//asi se comenta o con /* para bloques */
+//ya es codigo javascript
+alert("Hola mundo segunda forma")
+
+console.log("Hola mundo desde consola");
+
+document.write("<h1>hola mundo desde javascript</h1>");
